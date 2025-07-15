cac170601e702d8c5fc3e283632979af86a9cae9
AndyAtmadja03S <atmandy345@gmail.com>
2025-07-16 01:30:48 +1000

    fixed husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const test = 42
-  console.log("Hello world")
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <div className='bg-red-500 h-screen w-screen'>
+      <div className="bg-red-500 h-screen w-screen">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,7 +30,7 @@ function App() {
         </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
