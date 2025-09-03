1aa45183c611595a19d4079e4876fcbb7a47c4c9
Ryan Yensch <ry2005@outlook.com>
2025-09-04 02:56:39 +1000

    added automatic refreshing access token upon 401 error

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -6,4 +6,21 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use((res) => res,
+  async (err) => {
+    const original = err.config;
+    if (err.response?.status == 401 && !original._retry) {
+      original._retry = true;
+
+      try {
+        await api.post("/auth/refresh");
+        return api(original);
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
 export default api;
