c4e313c420c8bdb9a0d0dcb9d979eea0f9ab4a1a
AndyAtmadja03S <atmandy345@gmail.com>
2025-09-07 22:32:32 +1000

    Added protected route

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,8 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Register } from "./pages/Register";
 import { EventsPage } from "./pages/Events";
 import { Login } from "./pages/Login";
+
+import type { ReactNode } from "react";
+import { useUser } from "./hooks/useAuth";
+
+const ProtectedRoute: React.FC<{children?: ReactNode}> = ({ children }) => {
+  const { data: user } = useUser();
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
+  // Render the children (protected content) if authenticated
+  return children;
+};
 
 export const Router = () => {
   return (
@@ -10,7 +23,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register/>}/>
-        <Route path="/create-event" element={<EventsPage/>}/>
+        <Route path="/create-event" element={<ProtectedRoute><EventsPage/></ProtectedRoute>}/>
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
